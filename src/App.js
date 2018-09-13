f21f21af8c3f59a9e883177e1d7fe39996d9f28c
Patrick Powell <prp1277@gmail.com>
2018-09-13 16:09:32 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: ""
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("Success!");
+  }
+
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" onClick={this.handleClick}>
+          Click Me!
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
